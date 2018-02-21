cae608ab0e859c6dd60af9a6db2bd65ac54a9ae1
Ivan De Menezes <ivandmenezes@gmail.com>
2018-02-21 00:03:27 -0400

    Added dependencies

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+// Imports
 const Express = require('express');
 const App = Express();
+const Morgan = require('morgan');
+const Helmet = require('helmet');
+const BodyParser = require('body-parser');
 const Routes = require('./API/Routes');
 const PORT = process.env.PORT || 3000;
+
+// Middlewares
+if (App.get('ENV') === 'development') {
+    App.use(Morgan('dev'));
+} else {
+    App.use(Morgan('short'))
+}
+App.use(Helmet());
+App.use(BodyParser.json());
 
 App.use('/', Routes);
 App.listen(PORT, () => console.log(`API is running in port: ${PORT}`));
