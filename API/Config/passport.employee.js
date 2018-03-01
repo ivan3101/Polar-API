f60cf05f25a678ef5a340fac1875f9efe9d6c3ad
Ivan De Menezes <ivandmenezes@gmail.com>
2018-03-01 11:01:46 -0400

    Added passport strategy for employees

--- API/Config/passport.employee.js
+++ API/Config/passport.employee.js
@@ -1,18 +1,18 @@
 const Secret = require('./secret');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const ClientModel = require('../Models/client.model');
+const EmployeeModel = require('../Models/employee.model');
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = Secret.secret;
 
 module.exports = function(Passport) {
     Passport.use(new JwtStrategy(opts, async function(jwt_payload, done) {
-        const client = await ClientModel.findOne({
+        const employee = await EmployeeModel.findOne({
             '_id': jwt_payload._id,
             'isActive': true
         });
-        if (client) return done(null, client);
+        if (employee) return done(null, employee);
         else return done(null, false);
     }));
 };
